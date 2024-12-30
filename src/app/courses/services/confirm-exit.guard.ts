e0ed33097a436c60c39403e23ba3aa4bb8fe42cf
azhar35 <88520158+azhar35@users.noreply.github.com>
2024-12-29 20:51:11 -0500

    CanDeactivate

---
+++ src/app/courses/services/confirm-exit.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivate,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { CourseComponent } from "../course/course.component";
+import { Observable } from "rxjs";
+
+Injectable();
+export class ConfirmExitGuard implements CanDeactivate<CourseComponent> {
+  canDeactivate(
+    component: CourseComponent,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState: RouterStateSnapshot
+  ): boolean {
+    return component.confirmExit();
+  }
+}
