49b610267b462e6d4374724a636cd3472dfdea9c
azhar35 <88520158+azhar35@users.noreply.github.com>
2025-01-01 21:11:19 -0500

    router extra options

--- src/app/courses/services/lesson-detail.resolver.ts
+++ src/app/courses/services/lesson-detail.resolver.ts
@@ -20,7 +20,7 @@ export class LessonDetailResolver implements Resolve<LessonDetail> {
   ): Observable<LessonDetail> {
     // http://localhost:4200/courses/angular-router-course/lessons/17
 
-    const courseUrl = route.parent.paramMap.get("courseUrl"),
+    const courseUrl = route.paramMap.get("courseUrl"),
       lessonSeqNo = route.paramMap.get("lessonSeqNo");
 
     return this.courses.loadLessonDetail(courseUrl, lessonSeqNo);
