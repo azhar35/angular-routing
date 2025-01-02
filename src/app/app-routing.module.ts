88ed1e6b1f6c6083b538be0a4e8b2de749a277ac
azhar35 <88520158+azhar35@users.noreply.github.com>
2025-01-01 20:10:26 -0500

    secondary outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { AboutComponent } from "./about/about.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { CanLoadAuthGuard } from "./services/can-load-auth.guard";
 import { CustomPreloadingStrategy } from "./services/custom-preloading.strategy";
+import { ChatComponent } from "./chat/chat.component";
 
 const routes: Routes = [
   {
@@ -34,6 +35,11 @@ const routes: Routes = [
   {
     path: "about",
     component: AboutComponent,
+  },
+  {
+    path: "helpdesk-chat",
+    component: ChatComponent,
+    outlet: "chat",
   },
   {
     path: "**",
